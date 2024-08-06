46c4b763d1238ea51e60d71d09231455d97c4154
iqbalsyahbana <it.programmer2@triatra.co.id>
2024-08-06 11:04:16 +0700

    add shadcn effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,21 @@ import { Highlight } from "@/components/Highlight";
 import { Paragraph } from "@/components/Paragraph";
 import { Products } from "@/components/Products";
 import { TechStack } from "@/components/TechStack";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <Container>
       <span className="text-4xl">👋</span>
-      <Heading className="font-black">Hello there! I&apos;m Iqbal</Heading>
+      <TextGenerateEffect words="Hello there! I'm Iqbal" />
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a frontend developer that loves{" "}
-        <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
+        I&apos;m a frontend developer that loves <Highlight>building products</Highlight> and web apps that can impact millions of lives
       </Paragraph>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a junior software engineer with{" "}
-        <Highlight>1-2 years of experience</Highlight> building scalable web apps
-        that are performance optimized and good looking.
+        I&apos;m a junior software engineer with <Highlight>1-2 years of experience</Highlight> building scalable web apps that are performance optimized and good looking.
       </Paragraph>
-      <Heading
-        as="h2"
-        className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4"
-      >
+      <Heading as="h2" className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4">
         What I&apos;ve been working on
       </Heading>
       <Products />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
+
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -21,9 +32,23 @@ const config: Config = {
         primary: "var(--neutral-700)",
         secondary: "var(--neutral-500)",
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [require("@tailwindcss/typography"), addVariablesForColors],
+  plugins: [require("@tailwindcss/typography"), addVariablesForColors, require("tailwindcss-animate")],
 } satisfies Config;
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
@@ -38,4 +63,5 @@ function addVariablesForColors({ addBase, theme }: any) {
   });
 }
 
-export default config;
+
+export default config
